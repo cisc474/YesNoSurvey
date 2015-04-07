2f1baf6615fb4b283c927e7fc2a5b4f974f5bedd
Andy Novocin <andy@novocin.com>
2015-04-07 16:12:38 -0400

    adding server

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var router = express();
+var server = http.createServer(router);
+
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+//Insecure by the way
+router.use( express.bodyParser() );
+
+router.use(express.static(path.resolve(__dirname, 'src/client')));
+
+server.listen(22565, "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Upvote server listening at", addr.address + ":" + addr.port);
+});
